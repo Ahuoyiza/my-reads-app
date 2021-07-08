545180163ff28cafba9c92c10ba3094ab84f3cf7
sophia <sophieoyiza@gmail.com>
2021-07-08 06:04:43 +0100

    feat: :sparkles: project setup
    project setup
    
    none

---
+++ README.md
@@ -0,0 +1,24 @@
+# MyReads Project
+
+This is my solution for udacity read-me-app project for react nano degree,
+
+this file will be updated as neeed be
+
+
+## TL;DR
+
+To get started developing right away:
+
+* install all project dependencies with `npm install`
+* start the development server with `npm start`
+
+
+
+## Backend Server
+
+To simplify your development process, we've provided a backend server for you to develop against. The provided file [`BooksAPI.js`](src/BooksAPI.js) contains the methods you will need to perform necessary operations on the backend:
+
+* [`getAll`](#getall)
+* [`update`](#update)
+* [`search`](#search)
+
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React,{ useState, useEffect } from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import { Route, Link } from 'react-router-dom';
+import Search from './components/Search';
+import ListBooks from './components/ListBooks';
+
+const shelves = [
+  {
+    id:1,
+    name: 'Currently Reading',
+  },
+  {
+    id:2,
+    name: 'Want to Read'
+  },
+  {
+    id:3,
+    name: 'Read'
+  }
+];
+
+const App = () => {
+  const [books, setBooks] = useState();
+  const [search, setSearch] = useState();
+
+  const [error, setError] = useState(false);
+
+const bookData = async ()=> {
+    try {
+        const response = await BooksAPI.getAll()
+        console.log(response)
+    // const books = await response.json()
+    setBooks(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+    useEffect(() => {
+       bookData()
+    }, [])
+   
+
+    return (
+      <div className="app">
+        
+          <Search />
+          
+          <ListBooks />
+      </div>
+    )
+  
+}
+
+export default App
