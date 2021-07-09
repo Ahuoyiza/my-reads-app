26e226c52a40b5d954f75445156a77b4a82247ed
sophia <sophieoyiza@gmail.com>
2021-07-09 12:33:48 +0100

    feat: :sparkles: adds functions for book and change components
    adds functions for book and change components
    
    none

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React ,{ useContext }from 'react';
+import BookStoreContext from '../BookStore'
+import Changer from './Changer'
 
-const Book = () => {
+
+const Book = ({ book, search }) => {
+const { id, title, authors, shelf,  imageLinks } = book
+  const { books } = useContext(BookStoreContext)
+
+  const isBook = () => {
+    if (search) {
+      return books && books.find((book) => book.id === id)
+    }
+  }
+
+  const bookcover = imageLinks ? book.imageLinks.thumbnail : console.log(null)
+
+  const allAuthors = authors && authors.map((author) => <p key={author}>{author}</p>)
     return (
-        <div>
-            
-        </div>
+        <>
+            <li>
+                <div className='book'>
+                    <div className='book-top'>
+                    <div
+                        className='book-cover'
+                        style={{
+                        width: 128,
+                        height: 192,
+                        backgroundImage: `url(${bookcover})`,
+                        }}
+                    >
+                        <Changer shelf={shelf} book={isBook() !== undefined ? isBook() : book} search={search} />
+                    </div>
+                    </div>
+                    {title}
+                    </div>
+                    <div  className='book-authors'>
+                    {allAuthors}
+                    
+                </div>
+            </li>
+        </>
     )
 }
 
--- src/components/Changer.js
+++ src/components/Changer.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useContext, useState }  from 'react'
+import BookStoreContext from '../Store'
 
 
-const Changer = () => {
+
+
+const Changer = ({ book }) => {
+    const shelf = book.shelf ? book.shelf : 'none'
+  const { handleShelf } = useContext(BookStoreContext)
+  const [value, setValue] = useState(shelf)
+
+  const onChangeHandler = (e) => {
+    handleShelf(e.target.value, book)
+    setValue(e.target.value)
+  }
     return (
-        <div>
-            
+        <div className='book-shelf-changer'>
+            <select value={value} onChange={onChangeHandler}>
+                <option value='move' disabled>
+                Move to...
+                </option>
+                <option value='currentlyReading'>Currently Reading</option>
+                <option value='wantToRead'>Want to Read</option>
+                <option value='read'>Read</option>
+                <option value='none'>None</option>
+            </select>
         </div>
-    )
+    );
 }
 
 export default Changer
